707ea1fd77e1c86cffb5fe41fecaa1689921152c
nakajima1412 <nakajima1412@icloud.com>
2020-10-04 20:40:25 +0900

    video7

--- src/components/ModalView.vue
+++ src/components/ModalView.vue
@@ -8,8 +8,19 @@
 </template>
 
 <script>
+import { onBeforeUnmount } from 'vue'
 export default {
   setup(props, { emit }) {
+    let onkeydown = event => {
+      console.log(event.key)
+      if (event.key === 'Escape') {
+        emit('closeModal')
+      }
+    }
+    window.addEventListener('keydown', onkeydown)
+    onBeforeUnmount(() => {
+      window.removeEventListener('keydown', onkeydown)
+    })
     return {
       emit
     }
