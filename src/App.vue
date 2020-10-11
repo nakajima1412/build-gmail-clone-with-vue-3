fd000416b317b12691c19c58d0da269e0e17fe6a
nakajima1412 <nakajima1412@icloud.com>
2020-10-11 22:30:32 +0900

    video11

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <h1>VMail Inbox</h1>
+
+  <h1>{{ emailSelection.emails.size }} emails selected</h1>
+
   <Suspense>
     <template #default>
       <MailTable />
@@ -12,11 +15,17 @@
 
 <script>
 import MailTable from '@/components/MailTable.vue'
+import useEmailSelection from '@/composables/use-email-selection'
 
 export default {
   name: 'App',
   components: {
     MailTable
+  },
+  setup() {
+    return {
+      emailSelection: useEmailSelection()
+    }
   }
 }
 </script>
