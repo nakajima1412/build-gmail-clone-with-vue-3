51a1f5dd3a94cceeedfbaece6ba47e030442bec1
nakajima1412 <nakajima1412@icloud.com>
2020-11-09 00:41:37 +0900

    video14

--- src/components/BulkActionBar.vue
+++ src/components/BulkActionBar.vue
@@ -1,11 +1,30 @@
 <template>
-  <div>
-    <input
-      type="checkbox"
-      :checked="allEmailsSelected"
-      :class="[someEmailsSelected ? 'partial-check' : '']"
-      @click="bulkSelect"
-    />
+  <div class="bulk-action-bar">
+    <span class="checkbox">
+      <input
+        type="checkbox"
+        :checked="allEmailsSelected"
+        :class="[someEmailsSelected ? 'partial-check' : '']"
+        @click="bulkSelect"
+      />
+    </span>
+    <span class="buttons">
+      <button
+        @click="emailSelection.markRead"
+        :disabled="[...emailSelection.emails].every(e => e.read)"
+      >
+        Mark Read
+      </button>
+      <button
+        @click="emailSelection.markUnread"
+        :disabled="[...emailSelection.emails].every(e => !e.read)"
+      >
+        Mark Unread
+      </button>
+      <button @click="emailSelection.archive" :disabled="numberSelected === 0">
+        Archive
+      </button>
+    </span>
   </div>
 </template>
 
@@ -17,9 +36,10 @@ export default {
   setup(props) {
     let emailSelection = useEmailSelection()
     let numberSelected = computed(() => emailSelection.emails.size)
+    let numberEmails = props.emails.slice().length
     let allEmailsSelected = computed(() => numberSelected.value === props.emails.length)
     let someEmailsSelected = computed(() => {
-      return numberSelected.value > 0 && numberSelected.value < props.emails.length
+      return numberSelected.value > 0 && numberSelected.value < numberEmails
     })
     let bulkSelect = function() {
       if (allEmailsSelected.value) {
@@ -31,7 +51,9 @@ export default {
     return {
       allEmailsSelected,
       someEmailsSelected,
-      bulkSelect
+      bulkSelect,
+      emailSelection,
+      numberSelected
     }
   },
   props: {
